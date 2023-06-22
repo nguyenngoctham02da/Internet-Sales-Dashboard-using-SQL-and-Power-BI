c9293aefaf2cbef9f736582aef0c69f0a4040cc4
Tham Nguyen <98517280+nguyenngoctham02da@users.noreply.github.com>
2023-06-22 13:41:58 +0700

    Add files via upload

---
+++ DIM_Calendar.sql
@@ -0,0 +1,23 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --[DayNumberOfWeek],  
+  LEFT([EnglishDayNameOfWeek],3) as Day,
+  --[SpanishDayNameOfWeek], 
+  --[FrenchDayNameOfWeek], 
+  --[DayNumberOfMonth], 
+  --[DayNumberOfYear], 
+  --[WeekNumberOfYear],
+  Left([EnglishMonthName], 3) AS Month,   -- Useful for front end date navigation and front end graphs.
+  --[SpanishMonthName], 
+  --[FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --[CalendarSemester], 
+  --[FiscalQuarter], 
+  --[FiscalYear], 
+  --[FiscalSemester] 
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2019
---
+++ DIM_InternetSales.sql
@@ -0,0 +1,35 @@
+/*SELECT ProductKey, OrderDateKey, DueDateKey, ShipDateKey, CustomerKey, SalesOrderNumber, SalesAmount FROM FactInternetSales 
+where left(OrderDateKey,4)>=2019 order by OrderDateKey*/
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= 2019 -- Ensures we always only bring two years.
+ORDER BY
+  OrderDateKey ASC
